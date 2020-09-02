c30570ae752f13d51d3a51d9593e303246af9ca1
John Ratanaprasatporn <jratana1@gmail.com>
2020-09-02 19:54:11 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(integer)
-  if integer % 3 == 0
+  if integer % 3 == 0 && integer % 5 != 0
     return "Fizz"
-elsif integer % 5 == 0
+elsif integer % 5 == 0 && integer % 3 != 0
   return "Buzz"
+elsif integer % 5 == 0 && integer %3 == 0
+  return "FizzBuzz"
+else
+  return "nil"
 end
 
 end
